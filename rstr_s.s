1ea448a99dff912787d6983ad497d7a648bb5292
Max V-B <mvvillarrealblozis@dons.usfca.edu>
2023-09-20 23:09:04 +0000

    ver 2

--- rstr_s.s
+++ rstr_s.s
@@ -3,28 +3,36 @@
 .global strlen
 
 # Reverse a string iteratively
-# a0 - char *str
+# a0 - char *dst
+# a1 - char *str
 # t0 - int i
 # t1 - int j
 # t2 - int strlen(src)
 
 rstr_s:
-	addi sp, sp -16
+	addi sp, sp, -16
 	sd ra, (sp)
 
-	la t2, 
-
 	call strlen 
+	mv t2, a0 				# store strlen(src) in t2
 	
-	li t0, 0	# i = 0
+	li t0, 0				# i = 0
+	add t1, t2, -1			# j = src_Len - 1	
 
+loop:
+	beq t0, t2, done
 
+	lb t3, (a1)
+	sb t3, (a0)
 
+	addi a0, a0, 1
+	addi a1, a1, -1
+	addi t0, t0, 1
+	
+	j done
+done: 
+	sb zero, (a0)
+	
 	ld ra, (sp)
 	addi sp, sp, 16
     ret
-
-loop:
-
-done:
- 
