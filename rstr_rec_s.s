5584a8779b32817d1a9447e825542410125c4ba3
Max V-B <mvvillarrealblozis@dons.usfca.edu>
2023-09-22 03:05:47 +0000

    update

--- rstr_rec_s.s
+++ rstr_rec_s.s
@@ -1,4 +1,6 @@
 .global rstr_rec_s
+.global strlen
+
 
 # Reverse a string recursively
 
@@ -7,12 +9,35 @@
 # a2 - char *src 
 # a3 - int src_idx
 
-rstr_rec_s:
+rstr_rec_c:
+	addi sp, sp, -32
+	sd ra, (sp)
 
+	sd a0, (sp)
+	sd a1, 8(sp)
+	sd a2, 16(sp)
+	sd a3, 24(sp)
+
+	mv a0, a1
+	
+	call strlen
+
+	ld a0, (sp)
+	ld a1, 8(sp)
+	ld a2, 16(sp)
+	ld a3, 24(sp)
+
+	call rstr_rec_s
+	
+
+rstr_rec_s:
+	beq a3, zero, base_case
+	
 
 base_case:
-	
+
 
 done:
 
+	addi sp, sp, 32
 	ret 
