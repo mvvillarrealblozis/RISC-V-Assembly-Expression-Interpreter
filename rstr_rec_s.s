b0f9c6963bd009be238a138a4b4d0aa06cdf42d3
Max V-B <mvvillarrealblozis@dons.usfca.edu>
2023-09-24 18:58:32 +0000

    update

--- rstr_rec_s.s
+++ rstr_rec_s.s
@@ -1,6 +1,5 @@
-\.global rstr_rec_s
+.global rstr_rec_s
 .global strlen
-
 
 # Reverse a string recursively
 
@@ -9,35 +8,49 @@
 # a2 - char *src 
 # a3 - int src_idx
 
-rstr_rec_c:
+rstr_rec_s:
 	addi sp, sp, -32
 	sd ra, (sp)
 
-	sd a0, (sp)
-	sd a1, 8(sp)
-	sd a2, 16(sp)
-	sd a3, 24(sp)
+	sb a0, (sp)
+	sb a1, 8(sp)
+	sb a2, 16(sp)
+	sb a3, 24(sp)
 
-	mv a0, a1
+	mv a0, a2								# move source into a0 
 	
 	call strlen
 
-	ld a0, (sp)
-	ld a1, 8(sp)
-	ld a2, 16(sp)
-	ld a3, 24(sp)
+	lb a0, (sp)
+	lb a1, 8(sp)
+	lb a2, 16(sp)
+	lb a3, 24(sp)
+
+	call rstr_rec_func_s
+
+	ld ra, (sp)
+	
+rstr_rec_func_s:
+
+	lb t0, (a2)
+	
+	beq t0, zero, base_case
+
+	sb t0, (a0)	
+	
+	addi a1, a1, 1
+	addi a2, a2, -1
 
 	call rstr_rec_s
+
 	
-	
-rstr_rec_s:
-	beq a3, zero, base_case
+	j done 
 	
 
 base_case:
-
+	sb zero, (a0)
 
 done:
-
+	ld ra, (sp)
 	addi sp, sp, 32
 	ret 
