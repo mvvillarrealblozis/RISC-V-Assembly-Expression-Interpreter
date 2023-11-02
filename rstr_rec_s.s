c875ff96238c7ace6ed6c3dc529b4d3c274d378a
Max V-B <mvvillarrealblozis@dons.usfca.edu>
2023-11-02 02:53:53 +0000

    should be good

--- rstr_rec_s.s
+++ rstr_rec_s.s
@@ -1,4 +1,5 @@
 .global rstr_rec_s
+.global rstr_rec_func_s
 .global strlen
 
 # Reverse a string recursively
@@ -13,7 +14,6 @@ rstr_rec_s:
 	sd ra, (sp)
 	sd a0, 8(sp)
 	sd a1, 16(sp)
-	sd t0, 24(sp)
 	
 	mv a0, a1					# move source into a0 
 	
@@ -24,31 +24,42 @@ rstr_rec_s:
 
 	ld a0, 8(sp)
 	ld a1, 16(sp)
-	
-	mv a2, a1					# a2 = char *src 
-	mv a1, zero					# a1 = 0 
-	mv a3, t0 					# a3 = int src_idx
+
+	mv a2, a1
+	mv a1, zero
+	mv a3, t0
+
+	call rstr_rec_func_s
+
+	ld ra, (sp)
+	addi sp, sp, 32
+	ret
 		
-rec_step:
-	add t2, a2, a3				# t2 = src + src_idx
-	lb t1, (t2)
+
+rstr_rec_func_s:
+	addi sp, sp, -16
+	sd ra, (sp)
+
+	add t2, a2, a3				# t2 = src + src_idx	
+	lb t1, (t2)					# t1 = src[src_idx]
 
 	beq t1, zero, base_case		# if src[src_idx] == '\0'
 
-	add t3, a0, a1				# t3 = dst + dst_idx
-	sb t1, (t3)	
+	add t3, a0, a1				# t3 = dst + dst_idx 
+	sb t1, (t3)					# dst[dst_idx] = src[src_idx]
 
-	addi a1, a1, 1 				# dst_idx += 1
-	addi a3, a3, -16			# src_idx -= 1
+	addi a1, a1, 1				# dst_idx += 1
+	addi a3, a3, -1				# src_idx -= 1
 
-	j rec_step
+	call rstr_rec_func_s
+	
+	j done
 	
 base_case:
 	add t3, a0, a1 
 	sb zero, (t3)
 
 done:
-	ld t0, 24(sp)
 	ld ra, (sp)
-	addi sp, sp, 32
+	addi sp, sp, 16
 	ret
